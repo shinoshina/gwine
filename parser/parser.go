07ffcd62721c8238736c2023544dc7cb3aa5f78a
zhangran6311 <1982600638@qq.com>
2022-09-11 17:30:06 +0800

    place

--- parser/parser.go
+++ parser/parser.go
@@ -19,6 +19,17 @@ const (
 	CALL        // function(x)
 )
 
+var precedences = map[token.TokenType]int{
+	token.EQ:       EQUALS,
+	token.NEQ:      EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
+	token.PLUS:     SUM,
+	token.MINUS:    SUM,
+	token.SLASH:    PRODUCT,
+	token.ASTERISK: PRODUCT,
+}
+
 type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
@@ -37,7 +48,7 @@ type (
 func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
 	p.prefixParseFns[tokenType] = fn
 }
-func (p *Parser) registerinfix(tokenType token.TokenType, fn infixParseFn) {
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 	p.infixParseFns[tokenType] = fn
 }
 func New(l *lexer.Lexer) *Parser {
@@ -52,11 +63,32 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
+	p.infixParseFns = make(map[token.TokenType]infixParseFn)
+	p.registerInfix(token.PLUS, p.parseInfixExpression)
+	p.registerInfix(token.MINUS, p.parseInfixExpression)
+	p.registerInfix(token.SLASH, p.parseInfixExpression)
+	p.registerInfix(token.ASTERISK, p.parseInfixExpression)
+	p.registerInfix(token.EQ, p.parseInfixExpression)
+	p.registerInfix(token.NEQ, p.parseInfixExpression)
+	p.registerInfix(token.LT, p.parseInfixExpression)
+	p.registerInfix(token.GT, p.parseInfixExpression)
+
 	p.nextToken()
 	p.nextToken()
 	return p
 }
-
+func (p *Parser) peekPrecedence() int {
+	if p, ok := precedences[p.peekToken.Type]; ok {
+		return p
+	}
+	return LOWEST
+}
+func (p *Parser) curPrecedence() int {
+	if p, ok := precedences[p.curToken.Type]; ok {
+		return p
+	}
+	return LOWEST
+}
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
@@ -97,13 +129,22 @@ func (p *Parser) parseStatement() ast.Statement {
 		return p.parseExpressionStatement()
 	}
 }
-func (p *Parser) parseExpresstion(precedence int) ast.Expression {
+func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
 		p.noPrefixFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
+
+	for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
+		infix := p.infixParseFns[p.peekToken.Type]
+		if infix == nil {
+			return leftExp
+		}
+		p.nextToken()
+		leftExp = infix(leftExp)
+	}
 	return leftExp
 }
 func (p *Parser) parseIdentifier() ast.Expression {
@@ -126,7 +167,18 @@ func (p *Parser) parsePrefixExpression() ast.Expression {
 		Operator: p.curToken.Literal,
 	}
 	p.nextToken()
-	expression.Right = p.parseExpresstion(PREFIX)
+	expression.Right = p.parseExpression(PREFIX)
+	return expression
+}
+func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+	expression := &ast.InfixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+		Left:     left,
+	}
+	precedence := p.curPrecedence()
+	p.nextToken()
+	expression.Right = p.parseExpression(precedence)
 	return expression
 }
 func (p *Parser) parseLetStatement() *ast.LetStatement {
@@ -160,7 +212,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 }
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	stmt := &ast.ExpressionStatement{Token: p.curToken}
-	stmt.Expression = p.parseExpresstion(LOWEST)
+	stmt.Expression = p.parseExpression(LOWEST)
 
 	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,6 +6,7 @@ import (
 	"gwine/lexer"
 	"testing"
 )
+
 func TestLetStatement(t *testing.T) {
 	input := `
 	let five = 5  ;
@@ -21,10 +22,10 @@ func TestLetStatement(t *testing.T) {
 		t.Fatalf("ParseProgram return nil")
 	}
 	if len(pg.Statements) != 3 {
-		t.Fatalf("let statement nums wrong ,expected 3,got %v \n",len(pg.Statements))
+		t.Fatalf("let statement nums wrong ,expected 3,got %v \n", len(pg.Statements))
 	}
 
-	tests := []struct{
+	tests := []struct {
 		expectedIdentifier string
 	}{
 		{"five"},
@@ -32,13 +33,13 @@ func TestLetStatement(t *testing.T) {
 		{"foobar"},
 	}
 
-	for i ,tt := range tests{
-		
+	for i, tt := range tests {
+
 		stmt := pg.Statements[i]
-		lstmt,_ := stmt.(*ast.LetStatement)
-		
+		lstmt, _ := stmt.(*ast.LetStatement)
+
 		if lstmt.Name.Value != tt.expectedIdentifier {
-			t.Fatalf("test %v wrong , expected %v ,got %v ",i,tt.expectedIdentifier,stmt.TokenLiteral())
+			t.Fatalf("test %v wrong , expected %v ,got %v ", i, tt.expectedIdentifier, stmt.TokenLiteral())
 		}
 	}
 }
@@ -51,17 +52,17 @@ func TestIdentifierExpression(t *testing.T) {
 	p := New(l)
 	program := p.ParseProgram()
 
-	stmt , ok := program.Statements[0].(*ast.ExpressionStatement)
-	if !ok{
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
 		t.Fatalf("wtf??")
 	}
-	fmt.Printf("expression statement token %+v\n",stmt.Token)
+	fmt.Printf("expression statement token %+v\n", stmt.Token)
 
-	ident , ok := stmt.Expression.(*ast.Identifier)
+	ident, ok := stmt.Expression.(*ast.Identifier)
 	if !ok {
 		t.Fatalf("wtf2??")
 	}
-	fmt.Printf("identifier token %+v\nvalue %v\n",ident.Token,ident.Value)
+	fmt.Printf("identifier token %+v\nvalue %v\n", ident.Token, ident.Value)
 
 }
 
@@ -72,16 +73,16 @@ func TestIntegerLiteral(t *testing.T) {
 	p := New(l)
 	program := p.ParseProgram()
 
-	stmt , ok := program.Statements[0].(*ast.ExpressionStatement)
-	if !ok{
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
 		t.Fatalf("wtf??")
 	}
-	fmt.Printf("expression statement token %+v\n",stmt.Token)
-	ident , ok := stmt.Expression.(*ast.IntegerLiteral)
+	fmt.Printf("expression statement token %+v\n", stmt.Token)
+	ident, ok := stmt.Expression.(*ast.IntegerLiteral)
 	if !ok {
 		t.Fatalf("wtf2??")
 	}
-	fmt.Printf("identifier token %+v\nvalue %v\n",ident.Token,ident.Value)
+	fmt.Printf("identifier token %+v\nvalue %v\n", ident.Token, ident.Value)
 }
 func TestPrefixExpression(t *testing.T) {
 	input := "-115;"
@@ -90,19 +91,47 @@ func TestPrefixExpression(t *testing.T) {
 	p := New(l)
 	program := p.ParseProgram()
 
-	stmt , ok := program.Statements[0].(*ast.ExpressionStatement)
-	if !ok{
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
 		t.Fatalf("wtf??")
 	}
-	fmt.Printf("expression statement token %+v\n",stmt.Token)
-	ident , ok := stmt.Expression.(*ast.PrefixExpression)
+	fmt.Printf("expression statement token %+v\n", stmt.Token)
+	ident, ok := stmt.Expression.(*ast.PrefixExpression)
 	if !ok {
 		t.Fatalf("wtf2??")
 	}
-	fmt.Printf("identifier token %+v\noperator %v\n",ident.Token,ident.Operator)
-	num ,ok := ident.Right.(*ast.IntegerLiteral)
-	if !ok{
+	fmt.Printf("identifier token %+v\noperator %v\n", ident.Token, ident.Operator)
+	num, ok := ident.Right.(*ast.IntegerLiteral)
+	if !ok {
 		t.Fatalf("wtf??3")
 	}
-	fmt.Printf("identifier token %+v\nvalue %v\n",num.Token,num.Value)
+	fmt.Printf("identifier token %+v\nvalue %v\n", num.Token, num.Value)
+}
+
+func TestNodeLiteral(t *testing.T) {
+
+	input := `
+	5+5;
+	5+1+2;
+	5*1+2;
+	5+1*2;
+	5/2*1;
+	5/2+1-1;
+	!2*2+1/2;
+	-1*2+1-2;
+	-1+2+3;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	for i, _ := range program.Statements {
+		stmt, ok := program.Statements[i].(*ast.ExpressionStatement)
+		if !ok {
+			t.Fatalf("nmsl")
+		}
+		fmt.Println(stmt.String())
+	}
+
 }
